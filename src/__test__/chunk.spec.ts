8a646c0a93d81b44fb5f26f22ab040236d2af399
boong <taurusboong2@gmail.com>
2022-08-24 19:24:40 +0900

    Fix : fix spelling

--- src/__test__/chunk.spec.ts
+++ src/__test__/chunk.spec.ts
@@ -9,7 +9,7 @@ describe('chunk', () => {
     ]);
   });
 
-  it('another array case', () => {
+  it('if array can"t be split evenly, the final chunk will be the remaining elements', () => {
     const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
     expect(chunk(testArr, 3)).toStrictEqual([
       [1, 2, 3],
@@ -19,7 +19,7 @@ describe('chunk', () => {
     ]);
   });
 
-  it('if array can"t be split evenly, the final chunk will be the remaining elements', () => {
+  it('is another array case', () => {
     const testArr = [1, 2, 3, 4, 5];
     expect(chunk(testArr, 2)).toStrictEqual([[1, 2], [3, 4], [5]]);
   });
